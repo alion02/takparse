3e70e75e11d176f98a92d585b7581b7e9cf1ef9a
alion02 <alojczek@gmail.com>
2022-03-19 20:03:30 +0100

    Move `Piece` to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
 mod ptn;
 mod tps;
-mod util;
 
 pub use tps::{Color, ParseTpsError, Stack, Tps};
-pub use util::Piece;
+
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    str::FromStr,
+};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Piece {
+    Flat,
+    Wall,
+    Cap,
+}
+
+impl FromStr for Piece {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "" | "F" => Ok(Self::Flat),
+            "S" => Ok(Self::Wall),
+            "C" => Ok(Self::Cap),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Display for Piece {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            Self::Flat => "",
+            Self::Wall => "S",
+            Self::Cap => "C",
+        }
+        .fmt(f)
+    }
+}

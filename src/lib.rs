61a62b9e2f17eadd6dbcc63ba265a1d51670ce68
alion02 <alojczek@gmail.com>
2022-03-19 20:24:53 +0100

    Improve `Piece` parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,30 @@ mod tps;
 pub use tps::{Color, ParseTpsError, Stack, Tps};
 
 use std::{
+    error::Error,
     fmt::{Display, Formatter, Result as FmtResult},
     str::FromStr,
 };
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ParsePieceError {
+    TooLong,
+    BadChar,
+}
+
+impl Error for ParsePieceError {}
+
+impl Display for ParsePieceError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        use ParsePieceError::*;
+
+        match self {
+            TooLong => "piece consisted of multiple characters",
+            BadChar => "unknown piece character",
+        }
+        .fmt(f)
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Piece {
@@ -16,15 +37,23 @@ pub enum Piece {
 }
 
 impl FromStr for Piece {
-    type Err = ();
+    type Err = ParsePieceError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "" | "F" => Ok(Self::Flat),
-            "S" => Ok(Self::Wall),
-            "C" => Ok(Self::Cap),
-            _ => Err(()),
+        let mut chars = s.chars();
+
+        let c = chars.next();
+
+        if chars.next() != None {
+            Err(ParsePieceError::TooLong)?
         }
+
+        Ok(match c.unwrap_or('F') {
+            'F' => Self::Flat,
+            'S' => Self::Wall,
+            'C' => Self::Cap,
+            _ => Err(ParsePieceError::BadChar)?,
+        })
     }
 }
 
